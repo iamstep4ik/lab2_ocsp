2e5190991f15e762958c4eea4df16e5e8f46f523
Gleb Gorelov <qgorelov@gmail.com>
2024-09-19 23:56:06 +0300

    Fixed base pricing for lifting

--- calc.py
+++ calc.py
@@ -1,9 +1,11 @@
 class LoadCalc:
+    base_price = 2000
+
     def __init__ (self, weight, floor, elevator):
         self.weight = weight
         self.floor = floor
         self.elevator = elevator
     
     def price(self):
-        self.price = self.floor * self.weight // 100 * 300
+        self.price = self.floor * self.weight // 100 * 300 + self.base_price
         # За каждые 100кг груза на этаж по 300 рублей
