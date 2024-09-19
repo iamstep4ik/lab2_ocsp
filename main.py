480c6e26a67335a100c1a7ec13cf151a3dcfdefc
Gleb Gorelov <qgorelov@gmail.com>
2024-09-20 00:53:32 +0300

    Added loops for input

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from calc import LoadCalc
 
-weight = int(input("Введите вес груза: "))
-floor = int(input("Введите этаж на который требуется поднять груз: "))
-flag = bool(input("Введите возможно ли поднять груз на заданный этаж T/F: "))
+while True:
+    weight = int(input("Введите вес груза: "))
+    if weight <= 0:
+        print("Вес должен быть больше 0")
+    else:
+        break
 
-if weight <= 0:
-    print("Вес должен быть больше 0")
-if floor <= 1:
-    print("Этаж должен быть выше 1")
+while True:
+    floor = int(input("Введите этаж на который требуется поднять груз: "))
+    if floor <= 1:
+        print("Этаж должен быть выше 1")
+    else:
+        break
+
+flag = bool(input("Введите возможно ли поднять груз на заданный этаж T/F: "))
 
 new_calc  = LoadCalc(weight,floor,flag)
