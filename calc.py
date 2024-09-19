744e3a07e01dcdd5ec6463be7d38dcea22870632
Stepan Kovalev <lilop720@gmail.com>
2024-09-19 23:58:47 +0300

    Inputs were implemented

---
+++ calc.py
@@ -0,0 +1,14 @@
+from calc import LoadCalc
+print("Введите вес груза")
+weight = int(input())
+print("Введите этаж на который требуется поднять груз")
+floor = int(input())
+print("Введите возможно ли поднять груз на заданный этаж True/False")
+flag = bool(input())
+
+if weight <= 0:
+    print("Вес должен быть больше 0")
+if floor <= 1:
+    print("Этаж должен быть выше 1")
+
+new_calc  = LoadCalc(weight,floor,flag)
